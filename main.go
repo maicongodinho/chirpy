804c976047aee1a4a134d3214082e7b25ec8d874
Maicon Godinho <maicongodinho1@gmail.com>
2024-09-23 15:16:32 -0300

    Update with official solution

--- main.go
+++ main.go
@@ -6,24 +6,17 @@ import (
 )
 
 func main() {
-	// File system handler
-	fileServer := http.FileServer(http.Dir("."))
+	const filepathRoot = "."
+	const port = "8080"
 
-	// Create a new ServeMux
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
-	// Handle root endpoint with file system handler
-	mux.Handle("/", fileServer)
-
-	// Create a new http.Server struct
-	server := &http.Server{
-		Addr:    ":8080", // Define the address and port to listen on
-		Handler: mux,     // Use the ServeMux as the server's handler
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
-	// Start the server using ListenAndServe
-	log.Println("Starting server on :8080")
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("Server failed: %s", err)
-	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
